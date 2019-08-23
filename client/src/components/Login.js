6efff484e4f8609da0fa02450af7d8cf2c9f1e2c
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-08-23 11:25:18 -0500

    displaying colors in privateroute

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [user, setUser] = useState({
+    credentials : {
+      username: '',
+      password: ''
+    }
+  })
+
+  const handleChange = e => {
+    setUser({
+      credentials: {
+        ...user.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', user.credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+      })
+      .catch(err => console.log(err.response))
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+        <form onSubmit={login}>
+          <input
+            type="text"
+            name="username"
+            value={user.credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={user.credentials.password}
+            onChange={handleChange}
+          />
+          <button>Log in</button>
+        </form>
     </>
   );
 };
