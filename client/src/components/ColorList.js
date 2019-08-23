65a9efc6b971e059a80926d4e84085cfc03ca21d
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-08-23 13:04:42 -0500

    working edit and delete

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
+
 
 const initialColor = {
   color: "",
@@ -7,17 +8,34 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  console.log('colortoedit', colorToEdit)
 
+  
+ useEffect(() => {
+  const id = colors.map(color => color.id ) 
+  const itemInArr = colors.find(color => `${colorToEdit.id}` === id)
+  if(itemInArr) setColorToEdit(itemInArr)
+ }, [colorToEdit])
+ 
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
 
+
   const saveEdit = e => {
-    e.preventDefault();
+    //e.preventDefault();
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res);
+        updateColors(res.data)
+        setColorToEdit(initialColor)
+      })
+      .catch(err => console.log(err.response))
+    
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
@@ -25,13 +43,21 @@ const ColorList = ({ colors, updateColors }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        console.log(res);
+        updateColors(res.data)
+        setColorToEdit(initialColor)
+      })
+      .catch(err => console.log(err.response))
   };
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
-        {colors.map(color => (
+        {colors && colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
               <span className="delete" onClick={() => deleteColor(color)}>
